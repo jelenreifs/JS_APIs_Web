c5345150edbe1f07fd05a90b2c13c189fb0889e7
REIFS HERNANDO <ereifs@serikat.es>
2020-10-11 21:00:23 +0200

    Filter de cartas por tipo

--- Cartas_Magic/index.js
+++ Cartas_Magic/index.js
@@ -10,38 +10,47 @@ let types = "";
 let card = "";
 let i;
 
+let resultado;
+
+
 
 fetch(URLTYPES)
   .then(response => response.json())
   .then(data => {
       for (let i = 1; i <= data.types.length; i++) { 
-      type += `<option value="${[i]}">${data.types[i]}</option>`;
+      type += `<option value="${data.types[i]}">${data.types[i]}</option>`;
     }
 
       types += `
-        <select onchange="irTematica()">
-        <option value="${[i]}">${type}</option>
-        </select>
+        <select id="selector" onchange="capturaValor()">${type}</select>
       `;
-    document.getElementById("selector").innerHTML = types;
+    document.getElementById("filtro").innerHTML = types;
   });
 
 
+function capturaValor() {
+  let select = document.getElementById('selector');
+  let selectedOption = select.options[select.selectedIndex].value;
+  console.log(selectedOption);
 
-
-function irTematica() {
   fetch(URLCARDS)
     .then(response => response.json())
+    //.then(data => resultado = data.cards.filter(elemento => elemento.type == selectedOption))
     .then(data => {
-      for (let i = 0; i < data.cards.length; i++) {
-        console.log(data.cards[i].name)
-        
-        card += `
-        <h4>${data.cards[i].name}</h4>
-        <p>${data.cards[i].originalText}</p>
-        <img src="${data.cards[i].imageUrl}" alt="carta">`
-      }
-     document.getElementById("cartas").innerHTML = card; 
-    });
-}
+      resultado = data.cards.filter(elemento => elemento.types == selectedOption)
+      console.log(resultado);
+
+
+      card += `<h4>${resultado.cards.name}</h4>
+        <p>${resultado.cards.text}</p>
+        <img src="${resultado.cards.imageUrl}" alt="imagen">
+        `;
+     
+    })
+     document.getElementById("cartas").innerHTML = card;
+
+
+    }
+   
+    
 
--- Pokemon/index.js
+++ Pokemon/index.js
@@ -1,11 +1,75 @@
-let base = `https://pokeapi.co/api/v2`;
 
-let url = `https://pokeapi.co/api/v2/type/`;
+let urlItem = 'https://pokeapi.co/api/v2/item?offset=20&limit=20';
+let urlType = 'https://pokeapi.co/api/v2/type';
+let urlPokemons = 'https://pokeapi.co/api/v2/pokemon';
 
-let pokemons = [];
-let results = [];
 
-fetch(url).then(response => response.json()).then(data => {
-  pokemons = `<div>${data.results.type}<div>`;
-  document.getElementById("app").innerHTML = pokemons;
-});
+let pokemon = "";
+let tipoPokemon = "";
+
+
+/* Listado de Pokemons */
+/* fetch(urlItem)
+  .then(response => response.json())
+  .then(data => {
+   for (let i = 0; i < data.results.length; i++) { 
+      pokemon += `<p>${data.results[i].name}<div>`;   
+    }
+     document.getElementById("listado").innerHTML = pokemon;
+  });
+ */
+
+
+/* Seleccionar tipo de Pokemon, y mostrar 3 pokemons aleatorios que sean de ese tipo */
+
+fetch(urlType)
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.results.length; i++) { 
+      tipoPokemon += `<option value="${data.results[i].name}">${data.results[i].name}</option>`;
+    }
+    let pokemonFiltro = `<select id="selector" onchange="capturaValor()">${tipoPokemon}</select>`;
+    document.getElementById("filtro").innerHTML = pokemonFiltro; 
+  });
+
+
+function capturaValor() {
+  let select = document.getElementById('selector');
+  let selectedOption = select.options[select.selectedIndex];
+  //console.log(selectedOption.value);
+
+  fetch(urlPokemons)
+  .then(response => response.json())
+  .then(data => {
+    for (let i = 0; i < data.results.length; i++) { 
+      fetch(`https://pokeapi.co/api/v2/pokemo/${i}`)
+        .then(response => response.json())
+        .then(data => data.results.filter(elemento => elemento == selectedOption));
+      console.log(elemento);
+         }
+     
+    })
+  }
+
+
+
+ /*  for (let i = 0; i < data.result.length; index++) {
+    const element = array[index]; */
+    
+  
+
+  /* data.results.find(elemento => elemento.) */
+
+
+  
+
+
+
+
+
+  
+
+
+
+ 
+
